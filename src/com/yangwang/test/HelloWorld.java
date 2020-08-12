00c8d6b09bbeffb039e19962f6689d5da2fedadb
yangwang <yangwang@bonc.com.cn>
2020-08-13 05:12:06 +0800

    线程的两种创建方式

--- src/com/yangwang/test/HelloWorld.java
+++ src/com/yangwang/test/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("hello world");
+        for (int i=0; i < 100; i++){
+            System.out.println(i);
+        }
     }
-
 }
