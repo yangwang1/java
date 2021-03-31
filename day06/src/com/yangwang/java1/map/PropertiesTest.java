7076f3ac4b6edfa105d008337775924b8dff951e
yangwang <yangwang@bonc.com.cn>
2021-03-31 22:22:43 +0800

    File

--- day06/src/com/yangwang/java1/map/PropertiesTest.java
+++ day06/src/com/yangwang/java1/map/PropertiesTest.java
@@ -13,7 +13,21 @@ import java.util.Properties;
 public class PropertiesTest {
 
     //Properties:常用来处理配置文件。key和value都是String类型
+    //以project为根目录
     public static void main(String[] args) throws Exception{
+        Properties pros = new Properties();
+
+        FileInputStream fis = new FileInputStream("jdbc.properties");
+        pros.load(fis);//加载流对应的文件
+
+        String name = pros.getProperty("name");
+        String password = pros.getProperty("password");
+        System.out.println("name = " + name + ", password = " + password);
+    }
+
+    //以本module为根目录
+    @Test
+    public void test() throws Exception{
         Properties pros = new Properties();
 
         FileInputStream fis = new FileInputStream("jdbc.properties");
